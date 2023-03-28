0572b4407ad986823719ab976a76fb0dbe75463e
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2023-03-28 16:52:02 +0200

    Update BPL_TEST2_Batch_fmpy_explore.py
    Just changed NCP to ncp for compliance with PyFMI notation

--- BPL_TEST2_Batch_fmpy_explore.py
+++ BPL_TEST2_Batch_fmpy_explore.py
@@ -68,13 +68,13 @@ else:
 
 # Provide various opts-profiles
 if flag_type in ['CS', 'cs']:
-   opts_std = {'NCP': 500}
-   opts_data = {'NCP': 12}
-   opts_fast = {'NCP': 12}
+   opts_std = {'ncp': 500}
+   opts_data = {'ncp': 12}
+   opts_fast = {'ncp': 12}
 elif flag_type in ['ME', 'me']:
-   opts_std = {'NCP': 500}
-   opts_data = {'NCP': 12}
-   opts_fast = {'NCP': 12}
+   opts_std = {'ncp': 500}
+   opts_data = {'ncp': 12}
+   opts_fast = {'ncp': 12}
 else:    
    print('There is no FMU for this platform')
 
@@ -476,7 +476,7 @@ def simu(simulationTime=simulationTime, mode='Initial', options=opts_std, diagra
          validate = False,
          start_time = 0,
          stop_time = simulationTime,
-         output_interval = simulationTime/options['NCP'],
+         output_interval = simulationTime/options['ncp'],
          record_events = True,
          start_values = start_values,
          fmi_call_logger = None,
@@ -512,7 +512,7 @@ def simu(simulationTime=simulationTime, mode='Initial', options=opts_std, diagra
             validate = False,
             start_time = prevFinalTime,
             stop_time = prevFinalTime + simulationTime,
-            output_interval = simulationTime/options['NCP'],
+            output_interval = simulationTime/options['ncp'],
             record_events = True,
             start_values = start_values,
             fmi_call_logger = None,
