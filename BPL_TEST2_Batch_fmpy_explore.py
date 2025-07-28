9d84035e41f336209cde41abfedb23d6c6c431ac
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2025-07-28 08:52:31 +0200

    Update setup-files with BPL 2.3.1 for Linux

--- BPL_TEST2_Batch_fmpy_explore.py
+++ BPL_TEST2_Batch_fmpy_explore.py
@@ -33,6 +33,7 @@
 # 2024-10-24 - Update BPL 2.2.2 - GUI
 # 2024-11-07 - Update BPL 2.3.0
 # 2025-06-16 - Test MSL 4.1.0 with OpenModelica genreated FMU
+# 2025-07-28 - Update BPL 2.3.1
 #------------------------------------------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------------------------------------------
@@ -104,7 +105,7 @@ if flag_vendor in ['JM', 'jm']:
 elif flag_vendor in ['OM', 'om']:
    MSL_usage = '4.1.0 - used components: none' 
    MSL_version = '4.1.0'
-   BPL_version = 'Bioprocess Library version 2.3.0' 
+   BPL_version = 'Bioprocess Library version 2.3.1' 
 else:    
    print('There is no FMU for this platform')
 
